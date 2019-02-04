3a3e803ec6cc3c1951c241d6a4092bf59c8cafbd
Jopkirs <megaalexs@yahoo.com>
2019-02-04 13:02:43 +0300

    Initial commit

---
+++ src/main/java/com/kaigo/watchwithme/WatchWithMeApplication.java
@@ -0,0 +1,14 @@
+package com.kaigo.watchwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchWithMeApplication.class, args);
+    }
+
+}
+
